6b791ca35718b3e310a629a292db8b66cafc9ac9
xichen@blueplanet.com <xichen@blueplanet.com>
2020-04-02 12:42:02 +1100

    ad plan service

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
     public static class ErrorMsg {
         public static final String REQUEST_PARAM_ERROR = "Request Parameter Wrong";
         public static final String SAME_NAME_ERROR = "Same user name exists";
+        public static final String CAN_NOT_FIND_RECORD = "can not find record" ;
+        public static final String SAME_NAME_PLAN_ERROR = "exist same plane error";
     }
 }
